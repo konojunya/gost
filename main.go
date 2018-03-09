b685f8365804995d5ceea6c1d69345fd88db8061
konojunya <junya03dance@gmail.com>
2018-03-09 17:20:06 +0900

    できた！

--- main.go
+++ main.go
@@ -2,28 +2,43 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
 func main() {
+	flag.Parse()
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "4e0f44db361bf296dfe768f1395bf9bd172b7bd3"},
-	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
-
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	gist := &github.Gist{
+	body, err := ioutil.ReadFile("./test/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gist, _, err := client.Gists.Create(ctx, &github.Gist{
 		Description: github.String("hoge"),
 		Public:      github.Bool(true),
-	}
-	_, res, err := client.Gists.Create(ctx, gist)
+		Files: map[github.GistFilename]github.GistFile{
+			"index.html": github.GistFile{
+				Content: github.String(string(body)),
+			},
+		},
+	})
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println(res.StatusCode)
+	fmt.Println(*gist.HTMLURL)
 }
